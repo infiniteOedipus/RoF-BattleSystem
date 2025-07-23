841304173e0fe133cb2ddd5015df6a223f7fe037
infiniteOedipus <xeno101blade02@gmail.com>
2025-07-22 18:40:34 -0700

    working on adding pixi Manifest

--- .assetpack.js
+++ .assetpack.js
@@ -1,17 +1,16 @@
 // .assetpack.js
-import { pixiPipes } from '@assetpack/core/pixi';
+import { pixiManifest } from '@assetpack/core/manifest';
 
 export default {
     entry: './raw-assets',
     output: './public/assets',
     pipes: [
-        pixiPipes({
-        cacheBust: true,
-        resolutions: { default: 1, low: 0.5 },
-        compression: { jpg: false, png: true, webp: true },
-        texturePacker: { nameStyle: "short" },
-        audio: {},
-        manifest: { createShortcuts: true },
+        pixiManifest({
+            output: "manifest.json",
+            createShortcuts: false,
+            trimExtensions: false,
+            includeMetaData: true,
+            nameStyle: 'short'
         })
     ],
 };
